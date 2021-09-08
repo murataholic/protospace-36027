04e780eaf72c60f91c59b1fa682a079460cf6b92
murataholic <murataholic@gmail.com>
2021-09-09 04:26:35 +0900

    詳細ページの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+   
   def index
     @prototypes = Prototype.all
   end
@@ -16,9 +17,14 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+  
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
 end
