67ea32cba72aba9f3843b36f21eb8590642dad82
murataholic <murataholic@gmail.com>
2021-09-09 21:10:08 +0900

    アクセス範囲の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-   
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_prototype, only: [:show, :edit, :update]
+
   def index
     @prototypes = Prototype.all
   end
@@ -11,24 +13,24 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-      redirect_to root_path      
+      redirect_to action: :index
     else
       render :new
     end
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
   
   def edit
-    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user.id
+      redirect_to root_path
+    end
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
@@ -43,11 +45,13 @@ class PrototypesController < ApplicationController
     end
   end
 
-
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
+  
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
 end
